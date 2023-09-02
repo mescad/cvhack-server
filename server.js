84627b5e088c92ab1563091bff07c8a9b0aefd55
Dumitru Mesca <dumitrumesca@gmail.com>
2023-09-02 21:43:39 +0100

    added feedback loop

--- server.js
+++ server.js
@@ -1,17 +1,24 @@
 const express = require("express");
-const app = express()
-const PORT= process.env.PORT || 8080;
-const cors= require("cors")
-const apiRoutes = require('./routes/api-routes');
+const session = require("express-session");
+const app = express();
+app.use(
+  session({
+    secret: "your-secret-key",
+    resave: false,
+    saveUninitialized: true,
+    cookie: { secure: false }, // Set to true if using HTTPS
+  })
+);
 
-
-
+const PORT = process.env.PORT || 8080;
+const cors = require("cors");
+const apiRoutes = require("./routes/api-routes");
 
 app.use(express.json());
 
 app.use(cors());
 
-app.use('/', apiRoutes);
+app.use("/", apiRoutes);
 
 app.listen(PORT, () => {
   console.log(`running at http://localhost:${PORT}`);
