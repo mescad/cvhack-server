d6e6cfdd130dd8030919e063409954c925fb5451
Dumitru Mesca <dumitrumesca@gmail.com>
2023-09-06 15:17:41 +0100

    bugfix

--- server.js
+++ server.js
@@ -11,12 +11,16 @@ app.use(
 );
 
 const PORT = process.env.PORT || 8080;
+
 const cors = require("cors");
 const apiRoutes = require("./routes/api-routes");
 
 app.use(express.json());
 
-app.use(cors());
+const corsOptions = {
+  origin: process.env.FRONTEND_URL,  
+}
+app.use(cors(corsOptions));
 
 app.use("/", apiRoutes);
 
